7e8956b815eaa217bb266a66c747e3f3f8734263
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-10-18 14:11:06 -0300

    aula operadores

---
+++ aula07/script.js
@@ -0,0 +1,13 @@
+// var n = 3
+// n += 4
+// n = n + 4 // mesma coisa so q maior
+// n *= 4
+// console.log(n)
+
+// Operador incremento
+var x = 1
+x = x + 1 // tipo maior
+x += 1 // mais encurtado
+x ++ // mais encurtado ainda
+++ x // diferença local
+console.log(x)
