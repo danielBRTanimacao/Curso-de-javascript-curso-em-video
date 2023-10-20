b9c39ab24d1c898ca32621a171b399c008eba72a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-10-20 16:23:07 -0300

    operadores 2

---
+++ aula08/script.js
@@ -0,0 +1,36 @@
+// OPERADORES RELACIONAIS
+// var a = 2
+// var b = 4
+// console.log(a >= b)
+// var preco = 100.5
+// var preco2 = 234.1
+// var curso = 'JAVASCRIPT'
+// var idade = 19
+// n1 = 2
+// n2 = 3
+// console.log(`O preço ${preco.toLocaleString('PT-BR', {style: 'currency', currency: 'BRL'})} é maior ou igual a ${preco2.toLocaleString('PT-BR', {style: 'currency', currency: 'BRL'})} = ${preco>=preco2}`)
+// // não pesquisei como faz a indentação ksks
+// console.log(curso == 'javascript')
+// console.log(curso == curso.toLowerCase()) // deixa a str totalmente minuscula
+// console.log(idade > 18)
+// console.log(n1 != n2)
+
+// IDENTIDADE
+
+// 5 == 5 true
+// 5 == '5' true
+
+// operador total identidade
+
+// 5 === '5' false
+// 5 === 5 true
+
+// OPERADORES LOGICOS 
+// ! NEGAÇÃO
+// && CONJUNÇÃO 
+// || DISJUNÇÕES
+
+var a = 5
+var b = 8
+console.log(a > b && b % 2 == 0) // primeiro são os operadores aritimeticos
+console.log(a <= b || b / 2 == 0)
