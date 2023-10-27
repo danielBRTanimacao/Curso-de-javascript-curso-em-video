54d4f58ce6ac59ff98b1c66bf6e2fc134e7cb601
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-10-27 15:14:27 -0300

    js update

--- aula09/script.js
+++ aula09/script.js
@@ -1,1 +1,15 @@
 // DOM
+
+var paragrafo = window.document.getElementsByTagName('p')[1]
+var corpo = document.body
+var select_div = document.getElementById('numeros')
+
+// select_div.style.background = 'black'
+select_div.innerText = '9 + 2 = ?'
+
+// document.write(paragrafo.innerText)
+// document.write(paragrafo.innerHTML)
+// corpo.style.background = 'black'
+
+// paragrafo.style.color = 'orange'
+// document.write('Esta escrito assim: ' + paragrafo.innerText)
