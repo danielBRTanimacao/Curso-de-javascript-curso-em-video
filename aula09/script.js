d8a7bf1d7d0795d483f7e0d5e1cdaccd364c467e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-10-31 21:30:31 -0300

    DOM

--- aula09/script.js
+++ aula09/script.js
@@ -1,12 +1,17 @@
 // DOM
 
-var paragrafo = window.document.getElementsByTagName('p')[1]
-var corpo = document.body
-var select_div = document.getElementById('numeros')
+// var paragrafo = window.document.getElementsByTagName('p')[1]
+// var corpo = document.body
+// var select_div = document.getElementById('numeros')
+// var d = document.getElementsByName('msg')[0]
+
+var d = document.querySelector('p#numeros')
+
+d.style.background = 'blue'
 
 // select_div.style.background = 'black'
-select_div.innerText = '9 + 2 = ?'
-
+// select_div.innerText = '9 + 2 = ?'
+// d.innerText = 'bababui'
 // document.write(paragrafo.innerText)
 // document.write(paragrafo.innerHTML)
 // corpo.style.background = 'black'
