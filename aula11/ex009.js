29649daf96b8ec020c43fa43023e3aee1c02d77a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-04 19:21:13 -0300

    condicionais

---
+++ aula11/ex009.js
@@ -0,0 +1,17 @@
+// Condição composta
+
+var pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}...`)
+
+// if (pais == 'Brasil') {
+//     console.log('Brasileiro')   
+// } else {
+//     console.log('Estrangeiro!')
+// }
+
+if (pais != 'Brasil') { // mesma coisa
+    console.log('Estrangeiro!')   
+} else {
+    console.log('E dos nossos!')
+}
