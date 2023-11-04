5dc04874915fe7a6c5287521f3a87080f35f09cb
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-04 19:21:35 -0300

    condicionais + meus testes

---
+++ aula11/ex008.js
@@ -0,0 +1,39 @@
+// Fluxo jS deve ser em sequencia
+
+// CONDICIONAIS
+
+// if (condition) {
+    
+// }else {
+    
+// }
+
+var velocity = 80
+
+
+console.log(`A velocidade atual é ${velocity}km/h`)
+
+if (velocity > 60) { // Condição simples não tem se não
+    console.log(`Você ultrapassou a velocidade permitida 60Km/h. MULTADO!`)
+}
+
+// MEUS TESTES
+
+// var um = 1
+// var dois = 2
+// var other_dois = '2' // string
+// var tres = 3
+// var soma = tres + um
+
+// if (dois === other_dois) {
+//     console.log('Verdade')
+// } else {
+//     console.log('Falso')
+// }
+
+// if (tres + um == 4) {
+//     console.log('verdade')
+// } else {
+//     console.log('mintira')
+// }
+
