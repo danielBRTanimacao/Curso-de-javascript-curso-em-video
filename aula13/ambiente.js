3250f33416d475c6dac8251e75ad85a5d298c9a5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-11 10:23:21 -0300

    estrutura de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var count = 1
+
+do {
+    console.log(`Passo ${count}`)
+    count++ // mesma coisa que c += 1
+} while (count <= 6)
+
+// var count = 1
+
+// while (count <= 6) {
+//     console.log(`Passo ${count}`)
+//     count++ // mesma coisa que c += 1
+// }
+
+// MEUS TESTES
+// var pizzas = 10
+// var comeu = 0
+
+// while (pizzas > comeu) {
+//     console.log(`Você comeu ${comeu += 1}`)
+// }
